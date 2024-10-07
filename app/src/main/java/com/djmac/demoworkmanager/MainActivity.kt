c2de986e98b9041c7c603cd7161eb43fe782abf9
Digvijay <digvijay.machale@gmail.com>
2024-10-08 01:27:52 +0530

    One Time and Periodic Work Manager Demo

--- app/src/main/java/com/djmac/demoworkmanager/MainActivity.kt
+++ app/src/main/java/com/djmac/demoworkmanager/MainActivity.kt
@@ -1,20 +1,97 @@
 package com.djmac.demoworkmanager
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.work.Constraints
+import androidx.work.NetworkType
+import androidx.work.OneTimeWorkRequest
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkInfo
+import androidx.work.WorkManager
+import androidx.work.WorkRequest
+import com.djmac.demoworkmanager.databinding.ActivityMainBinding
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    companion object {
+        val TAG = "DemoWorkManager"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnOneTime.setOnClickListener {
+            startOneTimeWork()
         }
+
+        binding.btnPeriodic.setOnClickListener {
+
+            startPeriodicTimeRequest()
+        }
+    }
+
+    private fun startPeriodicTimeRequest() {
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .build()
+
+        /*
+        The minimum interval for periodic work is 15 minutes.
+        If you specify a shorter interval,
+        WorkManager will automatically adjust it to 15 minutes.
+        */
+        val uploadWorkRequest = PeriodicWorkRequestBuilder<UploadWorker>(
+            15,
+            TimeUnit.MINUTES
+        ).setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(applicationContext).enqueue(uploadWorkRequest)
+        Log.d(TAG, "Periodic work started")
+        observeWorkRequestStatus(uploadWorkRequest)
+    }
+
+    private fun startOneTimeWork(){
+
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresCharging(true)
+            .build()
+
+        val uploadWorkerRequest = OneTimeWorkRequestBuilder<UploadWorker>()
+            .setConstraints(constraints)
+            .build()
+
+        WorkManager.getInstance(applicationContext).enqueue(uploadWorkerRequest)
+        Log.d(TAG, "One time work started")
+        observeWorkRequestStatus(uploadWorkerRequest)
+    }
+
+    private fun observeWorkRequestStatus(workRequest: WorkRequest) {
+
+        WorkManager.getInstance(applicationContext).getWorkInfoByIdLiveData(workRequest.id)
+            .observe(this) { workInfo ->
+                if (workInfo != null) {
+                    when (workInfo.state) {
+                        WorkInfo.State.SUCCEEDED -> Log.d(TAG, "Work succeeded")
+                        WorkInfo.State.FAILED -> Log.d(TAG, "Work failed")
+                        WorkInfo.State.RUNNING -> Log.d(TAG, "Work is running")
+                        WorkInfo.State.ENQUEUED -> Log.d(TAG, "Work is enqueued")
+                        WorkInfo.State.CANCELLED -> Log.d(TAG, "Work is cancelled")
+                        WorkInfo.State.BLOCKED -> Log.d(TAG, "Work is cancelled")
+                    }
+                }
+            }
+
     }
 }
--- app/src/main/java/com/djmac/demoworkmanager/UploadWorker.kt
+++ app/src/main/java/com/djmac/demoworkmanager/UploadWorker.kt
@@ -1,12 +1,25 @@
 package com.djmac.demoworkmanager
 
 import android.content.Context
+import android.util.Log
 import androidx.work.Worker
 import androidx.work.WorkerParameters
+import com.djmac.demoworkmanager.MainActivity.Companion.TAG
 
 class UploadWorker(context: Context, workerParameters: WorkerParameters) :
     Worker(context, workerParameters) {
     override fun doWork(): Result {
-        TODO("Not yet implemented")
+        try {
+            uploadData()
+            return Result.success()
+        } catch (e: Exception) {
+            return Result.failure()
+        }
+    }
+
+    private fun uploadData() {
+        Log.d(TAG, "Uploading data to server...")
+        Thread.sleep(20000)
+        Log.d(TAG, "Data Uploaded Successfully")
     }
 }
